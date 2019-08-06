0c6138f6bc7d3dac753823e331d7c4ad4140d024
shiraishi <shiraishi.t0421@gmail.com>
2019-08-06 20:17:42 +0900

    modify

--- gan.py
+++ gan.py
@@ -1,6 +1,3 @@
-
-# coding: utf-8
-
 import numpy as np
 import torch
 import torch.nn as nn
@@ -9,102 +6,103 @@ import torch.optim as optim
 
 
 class Generator(nn.Module):
-    def __init__(self, in_size, out_size):
-        super(Generator, self).__init__()
-        self.fc1 = nn.Linear(in_size, 1024)
-        self.fc2 = nn.Linear(1024, 1024)
-        self.fc3 = nn.Linear(1024, out_size)
+  def __init__(self, in_size, out_size):
+    super(Generator, self).__init__()
+    self.fc1 = nn.Linear(in_size, 1024)
+    self.fc2 = nn.Linear(1024, 1024)
+    self.fc3 = nn.Linear(1024, out_size)
 
-    def forward(self, x):
-        x = F.relu(self.fc1(x), inplace=True)
-        x = F.relu(self.fc2(x), inplace=True)
-        x = torch.sigmoid(self.fc3(x))
-        return x
+  def forward(self, x):
+    x = F.relu(self.fc1(x), inplace=True)
+    x = F.relu(self.fc2(x), inplace=True)
+    x = torch.sigmoid(self.fc3(x))
+    return x
 
 
 class Discriminator(nn.Module):
-    def __init__(self, in_size):
-        super(Discriminator, self).__init__()
-        self.drop1 = nn.Dropout(0.8)
-        self.fc1 = nn.Linear(in_size, 256)
-        self.drop2 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(256, 256)
-        self.drop3 = nn.Dropout(0.5)
-        self.fc3 = nn.Linear(256, 1)
+  def __init__(self, in_size):
+    super(Discriminator, self).__init__()
+    self.drop1 = nn.Dropout(0.2)
+    self.fc1 = nn.Linear(in_size, 1024)
+    self.drop2 = nn.Dropout(0.5)
+    self.fc2 = nn.Linear(1024, 1024)
+    self.drop3 = nn.Dropout(0.5)
+    self.fc3 = nn.Linear(1024, 1)
 
-    def forward(self, x):
-        x = F.relu(self.fc1(self.drop1(x)), inplace=True)
-        x = F.relu(self.fc2(self.drop2(x)), inplace=True)
-        x = torch.sigmoid(self.fc3(self.drop3(x)))
-        return x
+  def forward(self, x):
+    x = F.relu(self.fc1(self.drop1(x)), inplace=True)
+    x = F.relu(self.fc2(self.drop2(x)), inplace=True)
+    x = torch.sigmoid(self.fc3(self.drop3(x)))
+    return x
 
 
-def train(dataloader, epoch, generator=None, discriminator=None):
+class GAN:
+  def __init__(self, dataloader):
+    self.dataloader = dataloader
     data, _ = next(iter(dataloader))
-    height = data.size()[2]
-    width = data.size()[3]
+    self.channel = data.size()[1]
+    self.height = data.size()[2]
+    self.width = data.size()[3]
+    self.in_size = 100
+    self.out_size = self.channel * self.height * self.width
+    self.g = Generator(self.in_size, self.out_size)
+    self.d = Discriminator(self.out_size)
+    self.g_optimizer = optim.Adam(self.g.parameters())
+    self.d_optimizer = optim.Adam(self.d.parameters())
+    self.g_losses = []
+    self.d_losses = []
 
-    # parameters
-    in_size = 100
-    out_size = height * width
+  def train(self, epoch):
 
     # models
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    if generator is None:
-        g = Generator(in_size, out_size).to(device)
-    else:
-        g = generator.to(device)
-    if discriminator is None:
-        d = Discriminator(out_size).to(device)
-    else:
-        d = discriminator.to(device)
+    self.g = self.g.to(device)
+    self.d = self.d.to(device)
 
     # optimizers
     criterion = nn.BCELoss()
-    g_optimizer = optim.Adam(g.parameters(), lr=0.0001)
-    d_optimizer = optim.Adam(d.parameters(), lr=0.0001)
 
     # training
-    g_losses = []
-    d_losses = []
     for i in range(epoch):
-        g_running_loss = 0
-        d_running_loss = 0
-        for _, (images, _) in enumerate(dataloader):
-            n = images.size()[0]
+      g_running_loss = 0
+      d_running_loss = 0
+      for _, (images, _) in enumerate(self.dataloader):
+        n = images.size()[0]
 
-            ### train generator ###
-            g_optimizer.zero_grad()
+        ### train generator ###
+        self.g_optimizer.zero_grad()
 
-            # generate data
-            fake_images = g(torch.rand((n, in_size), device=device))
-            fake_labels = torch.full((n, ), 1, device=device)
+        # generate data
+        fake_images = self.g(torch.rand((n, self.in_size), device=device))
+        fake_labels = torch.full((n, ), 1, device=device)
 
-            # backpropagation
-            g_loss = criterion(d(fake_images).view(-1), fake_labels)
-            g_running_loss += g_loss.item()
-            g_loss.backward()
-            g_optimizer.step()
+        # backpropagation
+        g_loss = criterion(self.d(fake_images).view(-1), fake_labels)
+        g_running_loss += g_loss.item()
+        g_loss.backward()
+        self.g_optimizer.step()
 
 
-            ### train discriminator ###
-            d_optimizer.zero_grad()
+        ### train discriminator ###
+        self.d_optimizer.zero_grad()
 
-            # generate data
-            fake_images = g(torch.rand((n, in_size), device=device)).detach()
-            fake_labels = torch.full((n, ), 0, device=device)
-            real_labels = torch.full((n, ), 1, device=device)
+        # generate data
+        fake_images = self.g(torch.rand((n, self.in_size), device=device)).detach()
+        fake_labels = torch.full((n, ), 0, device=device)
+        real_labels = torch.full((n, ), 1, device=device)
 
-            # optimize
-            d_fake_loss = criterion(d(fake_images).view(-1), fake_labels)
-            d_real_loss = criterion(d(images.view(n, -1).to(device)).view(-1), real_labels)
-            d_loss = d_fake_loss + d_real_loss
-            d_running_loss += d_loss.item()
-            d_loss.backward()
-            d_optimizer.step()
+        # optimize
+        d_fake_loss = criterion(self.d(fake_images).view(-1), fake_labels)
+        d_real_loss = criterion(self.d(images.view(n, -1).to(device)).view(-1), real_labels)
+        d_loss = 0.5 * (d_fake_loss + d_real_loss)
+        d_running_loss += d_loss.item()
+        d_loss.backward()
+        self.d_optimizer.step()
 
-        g_losses.append(g_running_loss)
-        d_losses.append(d_running_loss)
-        print("[%d] Generator: %f, Discriminator: %f" % (i, g_running_loss, d_running_loss))
+      g_running_loss /= len(dataloader)
+      d_running_loss /= len(dataloader)
+      self.g_losses.append(g_running_loss)
+      self.d_losses.append(d_running_loss)
+      print("[%d] Generator: %f, Discriminator: %f" % (i, g_running_loss, d_running_loss))
 
-    return g, d, np.array(g_losses), np.array(d_losses)
+    return
