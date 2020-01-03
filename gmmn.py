15b4dcd07956a34fc8f5f0cc89a9ed968f6ad077
shiraishi <shiraishi.t0421@gmail.com>
2020-01-04 06:13:09 +0900

    update gmmn

--- gmmn.py
+++ gmmn.py
@@ -1,119 +1,89 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 
 
 class Generator(nn.Module):
   def __init__(self, noise_size, out_size):
-    # The number of hidden nodes are unclear.
     super(Generator, self).__init__()
-    self.fc1 = nn.Linear(noise_size, 1024)
-    self.fc2 = nn.Linear(1024, 1024)
-    self.fc3 = nn.Linear(1024, 1024)
-    self.fc4 = nn.Linear(1024, 1024)
-    self.fc5 = nn.Linear(1024, out_size)
+    # The number of hidden nodes are roughly chosen.
+    self.model = nn.Sequential(
+        nn.Linear(noise_size, 1024),
+        nn.ReLU(),
+        nn.Linear(1024, 1024),
+        nn.ReLU(),
+        nn.Linear(1024, 1024),
+        nn.ReLU(),
+        nn.Linear(1024, 1024),
+        nn.ReLU(),
+        nn.Linear(1024, out_size),
+        nn.Sigmoid())
 
   def forward(self, x):
-    x = F.relu(self.fc1(x), inplace=True)
-    x = F.relu(self.fc2(x), inplace=True)
-    x = F.relu(self.fc3(x), inplace=True)
-    x = F.relu(self.fc4(x), inplace=True)
-    x = torch.sigmoid(self.fc5(x))
-    return x
+    return self.model(x)
 
 
-class MMDs(nn.Module):
-  def __init__(self, sigmas):
-    super(MMDs, self).__init__()
-    self.sigmas = sigmas
+def distance(X, Y):
+  X_norm = (X ** 2).sum(1).view(-1, 1)
+  Y_norm = (Y ** 2).sum(1).view(1, -1)
+  dist = X_norm + Y_norm - 2 * torch.mm(X, Y.t())
+  return dist
 
-  def dist(self, X, Y=None):
-    if Y is None:
-      Y = X
-    X_norm = (X ** 2).sum(1).view(-1, 1)
-    Y_norm = (Y ** 2).sum(1).view(1, -1)
-    dist = X_norm + Y_norm - 2 * torch.mm(X, Y.t())
-    return dist
 
-  def mmd(self, X, Y, sigma):
-    N = len(X)
-    M = len(Y)
-    xx = self.dist(X)
-    yy = self.dist(Y)
-    xy = self.dist(X, Y)
-    xx = torch.exp(- xx / (2 * sigma)).sum() / (N ** 2)
-    yy = torch.exp(- yy / (2 * sigma)).sum() / (M ** 2)
-    xy = torch.exp(- xy / (2 * sigma)).sum() / (N * M)
-    mmd = torch.sqrt(xx + yy - 2 * xy)
-    return mmd
-
-  def forward(self, X, Y):
-    mmds = 0
-    for sigma in self.sigmas:
-      mmds += self.mmd(X, Y, sigma)
-    return mmds
+def MMD(X, Y, sigma):
+  N = X.shape[0]
+  M = Y.shape[0]
+  xx = torch.exp(- distance(X, X) / (2 * sigma)).sum() / (N ** 2)
+  yy = torch.exp(- distance(Y, Y) / (2 * sigma)).sum() / (M ** 2)
+  xy = torch.exp(- distance(X, Y) / (2 * sigma)).sum() / (N * M)
+  mmd = torch.sqrt(xx + yy - 2 * xy)
+  return mmd
 
 
 class GMMN:
-  def __init__(self, dataloader,
-               noise_size=100,
-               noise_generator=None,
-               generator=None,
-               optimizer=None
-              ):
-    self.dataloader = dataloader
-    data, _ = next(iter(dataloader))
-    self.channel = data.size()[1]
-    self.height = data.size()[2]
-    self.width = data.size()[3]
-
+  # The dimensionality of noise and bandwidths are roughly chosen.
+  def __init__(self, img_size, noise_size=100, sigmas=(1, 2, 5, 10, 20, 50)):
     self.noise_size = noise_size
-    if noise_generator is None:
-      self.noise_generator = lambda n: torch.rand(n, self.noise_size).uniform_(-1, 1)
-    else:
-      self.noise_generator = noise_generator
+    self.sigmas = sigmas
 
-    self.out_size = self.channel * self.height * self.width
+    self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    self.generator = Generator(noise_size, img_size).to(self.device)
 
-    if generator is None:
-      self.generator = Generator(self.noise_size, self.out_size)
-    else:
-      self.generator = generator
-
-    # Authors use SGD with momentum.
-    if optimizer is None:
-      self.optimizer = optim.Adam(self.generator.parameters())
-    else:
-      self.optimizer = optimizer
-
+    # The learning rate and momentum are roughly chosen
+    self.optimizer = optim.SGD(self.generator.parameters(), lr=0.01, momentum=0.9)
     self.losses = []
 
-  def train(self, epoch):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    self.generator = self.generator.to(device)
-
-    # The bandwidths are unclear.
-    criterion = MMDs([1, 2, 5, 10, 20, 50])
+  def train(self, dataloader, epoch):
+    criterion = lambda X, Y: torch.stack([MMD(X, Y, sigma) for sigma in self.sigmas]).sum()
 
     for i in range(epoch):
       running_loss = 0
 
-      for _, (images, _) in enumerate(self.dataloader):
+      for _, (images, _) in enumerate(dataloader):
         n = images.size()[0]
-        images = images.view(n, -1).to(device)
 
         self.optimizer.zero_grad()
-        noise = self.noise_generator(n).to(device)
-        fake_images = self.generator(noise)
+        fake_images = self.generator(torch.rand(n, self.noise_size, device=self.device).uniform_(-1, 1))
+        real_images = images.view(n, -1).to(self.device)
 
-        loss = criterion(images, fake_images)
+        loss = criterion(real_images, fake_images)
         running_loss += loss.item()
         loss.backward()
         self.optimizer.step()
 
-      running_loss /= len(self.dataloader)
+      running_loss /= len(dataloader)
       self.losses.append(running_loss)
-      print("[%d] loss: %f" % (i, running_loss))
-
+      print("[%d] loss: %f" % (i + 1, running_loss))
     return
+
+  def set_state(self, state):
+    self.generator.load_state_dict(state["generator_state_dict"])
+    self.optimizer.load_state_dict(state["optimizer_state_dict"])
+    self.losses.extend(state["losses"])
+    return
+
+  def get_state(self):
+    state = {"generator_state_dict": self.generator.state_dict(),
+            "optimizer_state_dict": self.optimizer.state_dict(),
+            "losses": self.losses}
+    return state
