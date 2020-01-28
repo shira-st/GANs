4519efb7c38cf817187a33098739f897317f0952
shiraishi <shiraishi.t0421@gmail.com>
2020-01-29 08:23:21 +0900

    update gan

--- gan.py
+++ gan.py
@@ -44,75 +44,80 @@ class Discriminator(nn.Module):
 
 
 class GAN:
-  # The dimensionality of noise is roughly chosen.
-  def __init__(self, img_size, noise_size=100):
-    self.noise_size = noise_size
+  def __init__(self, data_size,
+               noise_generator=None,
+               generator=None,
+               discriminator=None,
+               g_optimizer=None,
+               d_optimizer=None,
+               device=None):
+    self.device = device
 
-    self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    self.generator = Generator(noise_size, img_size).to(self.device)
-    self.discriminator = Discriminator(img_size).to(self.device)
+    # The dimensionality of noise is roughly chosen.
+    if noise_generator:
+      self.noise_generator = lambda n: noise_generator(n).to(device)
+    else:
+      self.noise_generator = lambda n: torch.rand(n, 100, device=device)
+
+    if generator:
+      self.generator = generator.to(device)
+    else:
+      self.generator = Generator(self.noise_generator(1).size()[1], data_size).to(device)
+    if discriminator:
+      self.discriminator = discriminator.to(device)
+    else:
+      self.discriminator = Discriminator(data_size).to(device)
 
     # The learning rates and momentums are roughly chosen.
-    self.g_optimizer = optim.SGD(self.generator.parameters(), lr=0.01, momentum=0.9)
-    self.d_optimizer = optim.SGD(self.discriminator.parameters(), lr=0.01, momentum=0.9)
-    self.g_losses = []
-    self.d_losses = []
+    if g_optimizer:
+      self.g_optimizer = g_optimizer
+    else:
+      self.g_optimizer = optim.SGD(self.generator.parameters(), lr=0.01, momentum=0.9)
+    if d_optimizer:
+      self.d_optimizer = d_optimizer
+    else:
+      self.d_optimizer = optim.SGD(self.discriminator.parameters(), lr=0.01, momentum=0.9)
 
-  def train(self, dataloader, epoch):
+  def train(self, data):
     criterion = nn.BCELoss()
 
-    for i in range(epoch):
-      g_running_loss = 0
-      d_running_loss = 0
+    n = data.size()[0]
 
-      for _, (images, _) in enumerate(dataloader):
-        n = images.size()[0]
+    ### train discriminator ###
+    self.d_optimizer.zero_grad()
+    fake_data = self.generator(self.noise_generator(n))
+    fake_labels = torch.zeros((n, ), device=self.device)
+    real_labels = torch.ones((n, ), device=self.device)
 
-        ### train discriminator ###
-        self.d_optimizer.zero_grad()
-        fake_images = self.generator(torch.rand(n, self.noise_size, device=self.device))
-        real_images = images.view(n, -1).to(self.device)
-        fake_labels = torch.zeros((n, ), device=self.device)
-        real_labels = torch.ones((n, ), device=self.device)
+    d_fake_loss = criterion(self.discriminator(fake_data).view(-1), fake_labels)
+    d_real_loss = criterion(self.discriminator(data).view(-1), real_labels)
+    d_loss = d_fake_loss + d_real_loss
+    d_loss.backward()
+    self.d_optimizer.step()
 
-        d_fake_loss = criterion(self.discriminator(fake_images).view(-1), fake_labels)
-        d_real_loss = criterion(self.discriminator(real_images).view(-1), real_labels)
-        d_loss = d_fake_loss + d_real_loss
-        d_running_loss += d_loss.item()
-        d_loss.backward()
-        self.d_optimizer.step()
+    ### train generator ###
+    self.g_optimizer.zero_grad()
+    fake_data = self.generator(self.noise_generator(n))
+    fake_labels = torch.ones((n, ), device=self.device)
 
-        ### train generator ###
-        self.g_optimizer.zero_grad()
-        fake_images = self.generator(torch.rand(n, self.noise_size, device=self.device))
-        fake_labels = torch.ones((n, ), device=self.device)
+    g_loss = criterion(self.discriminator(fake_data).view(-1), fake_labels)
+    g_loss.backward()
+    self.g_optimizer.step()
+    return g_loss.item(), d_loss.item()
 
-        g_loss = criterion(self.discriminator(fake_images).view(-1), fake_labels)
-        g_running_loss += g_loss.item()
-        g_loss.backward()
-        self.g_optimizer.step()
-
-      g_running_loss /= len(dataloader)
-      d_running_loss /= len(dataloader)
-      self.g_losses.append(g_running_loss)
-      self.d_losses.append(d_running_loss)
-      print("[%d] Generator: %f, Discriminator: %f" % (i + 1, g_running_loss, d_running_loss))
-    return
+  def sample(self, n):
+    return self.generator(self.noise_generator(n))
 
   def set_state(self, state):
     self.generator.load_state_dict(state["generator_state_dict"])
     self.discriminator.load_state_dict(state["discriminator_state_dict"])
     self.g_optimizer.load_state_dict(state["g_optimizer_state_dict"])
     self.d_optimizer.load_state_dict(state["d_optimizer_state_dict"])
-    self.g_losses.extend(state["g_losses"])
-    self.d_losses.extend(state["d_losses"])
     return
 
   def get_state(self):
     state = {"generator_state_dict": self.generator.state_dict(),
-            "discriminator_state_dict": self.discriminator.state_dict(),
-            "g_optimizer_state_dict": self.g_optimizer.state_dict(),
-            "d_optimizer_state_dict": self.d_optimizer.state_dict(),
-            "g_losses": self.g_losses,
-            "d_losses": self.d_losses}
+             "discriminator_state_dict": self.discriminator.state_dict(),
+             "g_optimizer_state_dict": self.g_optimizer.state_dict(),
+             "d_optimizer_state_dict": self.d_optimizer.state_dict()}
     return state
